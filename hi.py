17c0c3d5be9c9d16811c33d36ae925a14c02d15d
suspicious <lexaxxl123@gmail.com>
2026-01-21 19:29:04 +0100

    Remove logging for start command
    Removed logging of start command with user details.

--- hi.py
+++ hi.py
@@ -4,6 +4,4 @@ def start_message(message):
     chat_id = message.chat.id
     user_name = message.from_user.first_name
 
-    logging.info(f'commands "start"| user_name -> {user_name}, chat_id -> {chat_id}')
-
     bot.send_message(chat_id, f'Hello {user_name}')
